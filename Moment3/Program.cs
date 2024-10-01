703ea47992ca5d88ded0c0c63e30d844784550d6
Hanin Farhan <haninfarhan96@gmail.com>
2024-10-01 12:50:00 +0200

    Add project files.

---
+++ Moment3/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+DT071G Programmering i c#.NET
+Hanin Farhan
+Moment 3 Objektorienterad c# 
+Uppgift - Skapa en gästbok:
+- Skapa en konsolapplikation
+- Inmatning av data
+- Skapa klasser
+- Felhantering och validering, inga tomma fält
+- Inmatning i form av två fält ska serialiseras/deserialiseras
+- Data ska lagras i json fil
+- Lägga till och ta bort data(inlägg) via konsolinmatning
+- Menyval skrivs om med console.Clear för varje förändring i gästboken
+*/
+namespace Moment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
